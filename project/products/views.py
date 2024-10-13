b3080cbe952fafdaf8472ef6784b05ece29a2e46
root <ahmedalgohary1170@gmail.com>
2024-10-13 04:50:57 +0300

    design api for product & category list

--- project/products/views.py
+++ project/products/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Category,Product
+from .serializers import CategorySerializer , ProductSerializer
+
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
